979e9b87d5a649e6a4e35a93f2053186cc584f70
slapab <sla.pab.dev@gmail.com>
2015-11-25 23:59:02 +0100

    added example to main how to declare and init the ring buffer

--- Sources/src/main.c
+++ Sources/src/main.c
@@ -4,6 +4,11 @@
 #include "ft801_gpu.h"    // FT801 registers 
 #include "ft801_api.h"    // include api functions
 
+#include "ringBuffer_api.h"
+
+#include <assert.h>
+
+
 volatile uint8_t value[8] ;
 volatile uint8_t cnt = 0 ;
 
@@ -25,6 +30,29 @@ void sleep( uint32_t delay )
 
 int main(void)
 {
+    
+    // declare the memory for ring buffer
+    uint8_t pBuff_test[16];
+    // Declare the descryptor for ring buffer
+    rbd_t rb_spi_rd_descr ;
+    
+    // declare the ring buffer structure and init it
+    rb_attr_t ringBuffSPI_read =
+                {
+                    sizeof( pBuff_test[0]),
+                    sizeof( pBuff_test ) / sizeof( pBuff_test[0]),
+                    (void*)&pBuff_test[0]
+                } ;
+    
+    // add defined ring buffer to the system and get the descriptor
+    if ( !ring_buffer_init(&rb_spi_rd_descr, &ringBuffSPI_read) )
+    {
+        assert(!"Can not init ring buffer") ;
+    }
+    
+    
+    
+    
     gpu_init_spi() ;
     
 
